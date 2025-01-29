1a17796e55212ca1f85f248343e71a975c97b225
Oleg <mamonov.1966@yandex.ru>
2025-01-29 12:24:01 +0300

    del imageButton file

--- config/createObjects.py
+++ config/createObjects.py
@@ -1,6 +1,6 @@
 from UI.classScreenGame import ScreenGame
 
-screen = ScreenGame(size = (1280, 768), 
+screen = ScreenGame(size = (1280, 720), 
                     caption = 'Game',
                     color = 'SteelBlue',
                     # icon = 'images/icon.jpg', # пример иконки
